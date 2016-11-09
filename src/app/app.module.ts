da75cfe819ebe3ba1ce61797c40ab2ca70dce664
Roy Bailey <rbailey@silvertouch.co.uk>
2016-11-09 13:21:28 +0000

    Added support for google maps.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+// Great article on getting maps working with ionic.  Out the box, it fails when you run on device.
+// Workaround and explanation here: https://forum.ionicframework.com/t/angular2-google-maps-in-ionic-2/65736 and code for that here: https://github.com/modularcoder/ionic2-angular2-google-maps-example/commit/e94819ee17df7bb959c20e10075f207603a7dbd3
+
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -6,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { IncidentDetailPage } from '../pages/incidentDetail/incidentDetail';
 import { TabsPage } from '../pages/tabs/tabs';
 import { incidentsAPI, Config } from '../shared/shared';
+import { AgmCoreModule } from 'angular2-google-maps/esm/core/index.js';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,8 @@ import { incidentsAPI, Config } from '../shared/shared';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({ apiKey: 'AIzaSyBbsOlMryAHu2ESwHHSwrDBIUU7fiENNoM'})
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/IncidentDetail/incidentDetail.ts
+++ src/pages/IncidentDetail/incidentDetail.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
 
+declare var window: any;
+
 @Component({
   selector: 'page-Incident-details',
   templateUrl: 'IncidentDetail.html'
@@ -9,9 +11,58 @@ import { NavController, NavParams } from 'ionic-angular';
 
 export class IncidentDetailPage {
   incidentDetail: any;
+  map: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-      this.incidentDetail = navParams.data;
+
+    this.incidentDetail = navParams.data;
+
+    let location = this.getIncidentLatLong(this.incidentDetail); 
+
+    this.map = {
+      lat: location.lat,
+      lng: location.long,
+      zoom: 12,
+      markerLabel: this.incidentDetail.Region 
+    };      
+  }
+
+  ionViewDidLoad(){
+
+  }
+
+  getDirections() { 
+    window.location = `geo:${this.map.lat},${this.map.lng};u=35`; 
+  }
+
+  private getIncidentLatLong(incident) {
+    let location = {
+      lat: 52.559615,
+      long: -1.786338
+    };
+
+    switch(incident.Region) {
+      case "UK":
+        location.lat = 52.559615;
+        location.long = -1.786338;
+        break;
+
+      case "ROW":
+        location.lat = 36.526386;
+        location.long = -116.883405;
+        break;
+
+      case "PJOBS":
+        location.lat = -7.962696;
+        location.long = -14.405716;
+        break;
+      
+      default:
+        location.lat = 48.129231;
+        location.long = 11.573763;
+        break;
+    }
+    return location;
   }
 
   statusColour() {
